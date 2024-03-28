8b78ef4bb235c015af4d0f14db82511f9d59d420
sushanto30 <sushantopal30@gmail.com>
2024-03-28 11:56:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ 
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       
+      <h1 className='text-2xl   font-extrabold'>Daily project</h1>
+       
+    </>
+  )
+}
+
+export default App
